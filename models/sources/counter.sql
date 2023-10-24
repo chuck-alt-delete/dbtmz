630ddc5748d22dc458a22d886699ea54277054e6
chuck-alt-delete <chuck@materialize.com>
2023-10-24 11:38:14 -0700

    add profiles.yml

--- models/sources/counter.sql
+++ models/sources/counter.sql
@@ -1,4 +1,9 @@
-{{ config(materialized='source') }}
+{{ config(
+    materialized='source',
+    post_hook = [
+        "CREATE DEFAULT INDEX IN CLUSTER {{target.cluster}} ON {{this}}"
+    ]
+    ) }}
 
 CREATE SOURCE {{this}} IN CLUSTER ingest
     FROM LOAD GENERATOR COUNTER
