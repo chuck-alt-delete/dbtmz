9bede55b221e14d6bd98a9acb78d1df227be0bd3
chuck-alt-delete <chuck@materialize.com>
2023-11-03 12:32:20 -0700

    minor fix

--- models/views/t_mv.sql
+++ models/views/t_mv.sql
@@ -2,7 +2,8 @@
   config(
     materialized = 'materialized_view',
     pre_hook = [
-      "CREATE TABLE IF NOT EXISTS {{ target.schema }}.t (id INT, content TEXT);",
+      "DROP TABLE IF EXISTS {{ target.schema }}.t;",
+      "CREATE TABLE {{ target.schema }}.t (id INT, content TEXT);",
       "INSERT INTO {{ target.schema }}.t VALUES (1, 'hi'), (2, 'hello');"
     ]
   )
